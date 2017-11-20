7e541e9f1a79fe1ff30035991f9f3eec243ac8d5
Sergey <go.for.broke1006@gmail.com>
2017-11-20 13:25:46 +0300

    Create check_mem_usage.cpp (only for linux)

---
+++ check_mem_usage.cpp
@@ -0,0 +1,45 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include <iostream>
+
+using namespace std;
+
+#define KYLOBYTES_TO_MEGABYTES ((float) 1 / 1024)
+
+int parseLine(char* line) {
+    // This assumes that a digit will be found and the line ends in " Kb".
+    int i = strlen(line);
+    const char* p = line;
+    while (*p < '0' || *p > '9') p++;
+    line[i - 3] = '\0';
+    i = atoi(p);
+    return i;
+}
+
+int getValue(){ //Note: this value is in KB!
+    FILE* file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL){
+        if (strncmp(line, "VmSize:", 7) == 0){
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return (int)(result * KYLOBYTES_TO_MEGABYTES);
+}
+
+int main() {
+    unsigned long l = 100000000;
+    int* r = new int[l];
+    for (int i = 0; i < l; ++i) {
+        r[i] = rand();
+    }
+
+    cout << getValue() << " Mb" << endl;
+
+    return 0;
+}
