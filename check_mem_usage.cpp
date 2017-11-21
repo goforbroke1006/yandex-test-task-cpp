9a5c9c63096dbae034e4da8f995ba1987a6e9742
kugert <artem.strohin@gmail.com>
2017-11-21 19:20:35 +0200

    remade calculations

--- check_mem_usage.cpp
+++ check_mem_usage.cpp
@@ -18,13 +18,9 @@
 #include "stdlib.h"
 #include "string.h"
 #include <iostream>
-<<<<<<< HEAD
 #include <iomanip>
 #include <chrono>
-=======
 #include <iomanip>
-#include <ctime>
->>>>>>> 0382e8df3fc85c006ee5f70ff71f783f597e9031
 
 using namespace std;
 
@@ -62,40 +58,21 @@ float getRAMUsage() {
 #endif
 }
 
-<<<<<<< HEAD
 int main() {
     auto begin_time =  chrono::high_resolution_clock::now();
-=======
-int main() {
-    float start_time =  clock()/1000.0;
->>>>>>> 0382e8df3fc85c006ee5f70ff71f783f597e9031
+
     unsigned long l = 100000;
     int* r = new int[l];
     for (unsigned long i = 0; i < l; ++i) {
         r[i] = rand();
     }
-<<<<<<< HEAD
-    unsigned long mas[l];
-    for (unsigned long k=0; k < l; ++k) {
-        mas[k] = rand();
-    }
+
     //cout << getValue() << " Mb" << endl;
-    cout << std::fixed << setw(11) << setprecision(6) << getValue() << " Mb" << endl;
+    cout << std::fixed << setw(11) << setprecision(6) << getRAMUsage() << " Mb" << endl;
 
     auto end_time = chrono::high_resolution_clock::now();
 
     cout << "spent time: " << chrono::duration_cast<chrono::milliseconds>(end_time-begin_time).count()<< "ms" << endl;
-=======
-
-    //cout << getRAMUsage() << " Mb" << endl;
-    cout << std::fixed << setw(11) << setprecision(6) << getRAMUsage() << " Mb" << endl;
-
-    float end_time = clock()/1000.0;
-
-    cout << "start time: " << start_time << endl;
-    cout << "end time: " << end_time << endl;
-    cout << "spent time: " << end_time - start_time << endl;
->>>>>>> 0382e8df3fc85c006ee5f70ff71f783f597e9031
 
     delete(r);
 
